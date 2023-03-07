4ac627a32fc350146dc43d5e3410c6c6ae346587
lab24 <kanonlas_r@cmu.ac.th>
2023-03-07 17:48:47 +0700

    edited

--- lab24.cpp
+++ lab24.cpp
@@ -28,6 +28,72 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	double simag = (real*c.imag)+(imag*c.real);
+	double sreal = (real*c.real)-(imag*c.imag);
+	return ComplexNumber(sreal,simag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	double sreal = (real*c.real)+(imag*c.imag);
+	double simag = (imag*c.real) - (real*(c.imag));
+	double x = pow(c.real,2)+ pow(c.imag,2);
+	return ComplexNumber(sreal/x,simag/x);
+}
+
+ostream & operator <<(ostream &os, const ComplexNumber &c){
+	if(c.real != 0 && c.imag > 0) return os << c.real << "+" << c.imag << "i" ;
+	else if(c.real != 0 && c.imag < 0) return os << c.real << c.imag << "i" ;
+	else if(c.real == 0 && c.imag != 0) return os << c.imag << "i" ;
+	else if(c.real != 0 && c.imag == 0) return os << c.real;
+	else return os << c.imag;
+}
+
+bool  ComplexNumber::operator==(const ComplexNumber &c){
+ if(real == c.real && imag == c.imag){
+  return true;
+ }else{
+	return false;
+ }
+}
+
+ComplexNumber operator+(double s, const ComplexNumber &c){
+	return ComplexNumber(s+c.real,+c.imag);
+}
+
+ComplexNumber operator-(double s, const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber operator*(double s, const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+
+ComplexNumber operator/(double s, const ComplexNumber &c){
+	double x = pow(c.real,2);
+	double y = pow(c.imag,2);
+	double z = x + y;
+	return ComplexNumber(s*c.real/z,s*(-c.imag)/z);
+
+}
+
+ComplexNumber operator==(double s,const ComplexNumber &c){
+ if( s == c.real && s == c.imag){
+  return true;
+ }else{
+	return false;
+ }
+}
+
+double ComplexNumber::abs(){
+    return sqrt(pow(real,2) + pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	double pi = 2*asin(1.0);
+	return atan2(imag,real)* 180 / pi;
+	}
+
 //Write your code here
 
 int main(){
